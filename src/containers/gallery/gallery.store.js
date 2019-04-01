876810ef11cd65f55cb6388288651e290d39d05e
Gilad takoni <gilad1987@gmail.com>
2019-04-01 20:45:13 +0300

    fix random func

--- src/containers/gallery/gallery.store.js
+++ src/containers/gallery/gallery.store.js
@@ -1,17 +1,41 @@
-import {decorate, observable, action, computed} from "mobx";
+import {decorate, observable, action} from "mobx";
 
 class GalleryStore {
 
     isGrayMode = false;
     images = [];
+    currentImages = [];
+    countGalleryImages = 30;
 
-    async getImages() {
-        const response = await fetch('https://picsum.photos/list');
-        const images = await response.json();
-        this.images = images.slice(0, 30);
+    async fetchImages(url = 'https://picsum.photos/list') {
+        const response = await fetch(url);
+        this.images = await response.json();
+        this.images.map(image => {
+            image.key = Math.random();
+            return image;
+        });
+        this.setRandomImages();
     }
 
-     toggleGrayMode() {
+    setRandomImages(num = this.countGalleryImages) {
+        const entriesNumber = Math.min(num, this.images.length);
+        const indexes = [];
+        const images = [];
+        let index;
+
+        for (let i = 0; i < entriesNumber; i++) {
+
+            do {
+                index = Math.floor(Math.random() * this.images.length);
+            } while (indexes.includes(index));
+
+            indexes.push(index);
+            images.push(this.images[index]);
+        }
+        this.currentImages = images;
+    }
+
+    toggleGrayMode() {
         this.isGrayMode = !this.isGrayMode;
     }
 
@@ -20,8 +44,10 @@ class GalleryStore {
 decorate(
     GalleryStore, {
         images: observable,
+        currentImages: observable,
         isGrayMode: observable,
         toggleGrayMode: action,
+        setRandomImages: action,
     }
 );
 
